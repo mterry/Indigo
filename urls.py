1b0660f48a40bb89c10f09071ca570ee86bc1e3c
Matthew A. Terry <mat@timepiecestudios.net>
2011-10-29 22:55:27 -0400

    Finished roughing out the url structure, and roughed in the views that will be needed.

--- urls.py
+++ urls.py
@@ -1,30 +1,30 @@
 from django.conf.urls.defaults import patterns, include, url
 
-urlpatterns = patterns('',
+urlpatterns = patterns('indigo.views',
     # Examples:
-    # url(r'^/', 'indigo.views.main'),
-    # url(r'^login/', 'indigo.views.user_auth', name='login'),
-    # url(r'^logout/', 'indigo.views.user_auth', name='logout'),
-    # url(r'^create_user/', 'indigo.views.user_auth', name='create_user'),
+    # url(r'^/', 'main'),
+    # url(r'^login/', 'user_auth', name='login'),
+    # url(r'^logout/', 'user_auth', name='logout'),
+    # url(r'^create_user/', 'user_auth', name='create_user'),
 
     # PROJECT URLS
-    # url(r'^projects/$', 'indigo.views.projects_list', name='public'),
+    # url(r'^projects/$', 'projects_list', name='public'),
     # Example url for the following:
     #   http://example.com/indigo/projects/bob/
-    # url(r'^projects/(?P<username>\w+)/$', 'indigo.views.projects_list', name='user_projects'),
+    # url(r'^projects/(?P<username>\w+)/$', 'projects_list', name='user_projects'),
     # Example url for the following:
     #   http://example.com/indigo/projects/23/
-    # url(r'^projects/(?P<project_id>\d+)/$', 'indigo.views.projects_detail'),
+    # url(r'^projects/(?P<project_id>\d+)/$', 'projects_detail'),
 
     # ITERATION URLS
     # Example url for the following:
     #   http://example.com/indigo/projects/23/iterations/
-    # url(r'^projects/(?P<project_id>\d+)/iterations/$', 'indigo.views.iterations_list'),
-    # url(r'^projects/(?P<project_id>\d+)/iterations/(?P<iteration_id>\d+)/$', 'indigo.views.iterations_detail', name='by_id'),
-    # url(r'^projects/(?P<project_id>\d+)/iterations/(?P<iteration_name>\w+)/$', 'indigo.views.iterations_detail', name='by_name'),
+    # url(r'^projects/(?P<project_id>\d+)/iterations/$', 'iterations_list'),
+    # url(r'^projects/(?P<project_id>\d+)/iterations/(?P<iteration_id>\d+)/$', 'iterations_detail'),
 
     # TASK URLS
     # Example url for the following:
     #   http://example.com/indigo/projects/23/iterations/tasks/
-    # url(r'^projects/(?P<project_id>\d+)/iterations/tasks/$', 'indigo.views.tasks_list')
+    # url(r'^projects/(?P<project_id>\d+)/iterations/tasks/$', 'tasks_list'),
+    # url(r'^projects/(?P<project_id>\d+)/iterations/tasks/(?P<task_id>\d+)/$', 'tasks_detail'),
 )
--- views.py
+++ views.py
@@ -1,1 +1,25 @@
+from django.http import HttpResponse
 # Create your views here.
+
+def user_auth(request):
+  return null
+
+def projects_list(request, username):
+  return null
+
+def projects_detail(request, project_id):
+  return null
+
+def iterations_list(request, project_id):
+  return null
+
+def iterations_detail(request, project_id, iteration_id):
+  return null
+
+def tasks_list(request, project_id, iteration_id):
+  return null
+
+def tasks_detail(request, project_id, iteration_id, task_id):
+  return null
+
+
