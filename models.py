aea2d474a4ab01c8ebc876a1ac54cc41966d8676
Matthew A. Terry <mat@timepiecestudios.net>
2011-10-29 23:25:40 -0400

    Fixed models.py. It should compile now.

--- models.py
+++ models.py
@@ -1,30 +1,30 @@
 from django.db import models
-from django.contrib.auth import models.User
+from django.contrib.auth.models import User
 
 class Project(models.Model):
-  name = models.charField(max_length=50)
-  description = models.textField()
-  velocity = integerField()
-  task_point_timescale = positiveIntegerField()
+  name = models.CharField(max_length=50)
+  description = models.TextField()
+  velocity = models.IntegerField()
+  task_point_timescale = models.PositiveIntegerField()
   collaborators = models.ManyToManyField(User)
 
   def __unicode__(self):
     return self.name
 
 class Iteration(models.Model):
-  name = models.charField(max_length=50)
-  number = models.positiveIntegerField()
-  velocity = models.integerField()
+  name = models.CharField(max_length=50)
+  number = models.PositiveIntegerField()
+  velocity = models.IntegerField()
   project = models.ForeignKey(Project)
   
   def __unicode__(self):
     return self.name
 
 class Task(models.Model):
-  name = models.charField(max_length=50)
-  description = models.textField()
-  points = models.positiveIntegerField()
-  assigned_to = models.ForeignKey(UserProfile, null=True, on_delete=models.SET_NULL)
+  name = models.CharField(max_length=50)
+  description = models.TextField()
+  points = models.PositiveIntegerField()
+  assigned_to = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
   iteration = models.ForeignKey(Iteration)
 
   def __unicode__(self):
