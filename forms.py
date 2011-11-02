95e819017510a5d17f65289bb61fe555e147ae66
Matthew A. Terry <mat@timepiecestudios.net>
2011-11-02 19:44:29 -0400

    Attempting to solve the bug in forms.py

--- forms.py
+++ forms.py
@@ -10,14 +10,20 @@ class CreateIterationForm(forms.Form):
   name = forms.CharField(max_length=50, required=False)
   number = forms.IntegerField(min_value=1, required=True)
 
-class CreateTaskForm(forms.Form, project):
+class CreateTaskForm(forms.Form):
+  def __init__(self, project):
+    self.project = project
+
   name = forms.CharField(max_length=50, required=True)
   description = forms.CharField(required=False)
   points = forms.IntegerField(min_value=1, required=True)
-  assigned_to = forms.ChoiceField(choices=project.get_collaborators(), required=False)
+  assigned_to = forms.ChoiceField(choices=self.project.get_collaborators(), required=False)
 
-class ModifyTaskForm(forms.Form, project):
+class ModifyTaskForm(forms.Form):
+  def __init__(self, project):
+    self.project = project
+
   description = forms.CharField(required=False)
   points = forms.IntegerField(min_value=1, required=True)
-  assigned_to = forms.ChoiceField(choices=project.get_collaborators(), required=False)
+  assigned_to = forms.ChoiceField(choices=self.project.get_collaborators(), required=False)
   closed = forms.BooleanField(requird=False)
